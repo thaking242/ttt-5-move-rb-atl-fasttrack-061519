840173c6088257d5cd3eed4245ec2a23e4cf9c27
Charleese burns <charleeseburns1@yahoo.com>
2019-06-16 18:19:30 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+board = Array.new(9)
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +9,20 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index
+  puts "Where would you like to go?"
+  input = gets
+  input.to_i - 1 
+  end  
+  
+#board, input_to_index, desired character
+def move(array,index,char)
+  array[index]= char
+end
+move(board,input_to_index,"X")
+
+display_board(board)
+
+move(board,input_to_index,"O")
+ 
+display_board(board)
